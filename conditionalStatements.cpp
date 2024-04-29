0a78d2b6109371769557ef6006a32d318ad6622d
Sameer Basha <sameerndl28@gmail.com>
2024-04-29 13:49:54 +0530

    Lecture 3

---
+++ conditionalStatements.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter n : "<<"\n";
+    cin>>n;
+
+    //if ststement
+    if(n>0){
+        cout<<n<<" is positive\n";
+    }
+
+    //if else statement
+    if(n>0){
+        cout<<n<<" is positive\n";
+    }
+    else{
+        cout<<n<<" is negative\n";
+    }
+
+    //else if statament
+    if(n>0){
+        cout<<n<<" is postive\n";
+    }
+    else if(n<0){
+        cout<<n<<" is negative\n";
+    }
+    else{
+        cout<<n<<" is zero\n";
+    }
+
+
+}
