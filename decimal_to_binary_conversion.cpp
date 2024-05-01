73d286867c858618b1e3a7c9dec72212d5f4c336
Sameer Basha <sameerndl28@gmail.com>
2024-05-01 06:18:41 +0530

    Lecture 6

---
+++ decimal_to_binary_conversion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+
+    float ans=0;
+    int i=0;
+    while(n!=0)
+    {
+        int bit=(n&1);
+        ans=(bit*pow(10,i)) + ans;
+        n=n>>1;
+        i++;
+    }
+
+    cout<<"Binary Representation of "<<n<<" is "<<ans;
+}
