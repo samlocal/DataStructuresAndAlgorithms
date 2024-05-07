ea4c8fdae1500694a4b4812ffa40d9d4906f95e1
Sameer Basha <sameerndl28@gmail.com>
2024-05-07 18:25:29 +0530

    Arrays Introduction and Problems

---
+++ ArraysDSA/Array_16_move_zaros.cpp
@@ -0,0 +1,9 @@
+// void moveZeros(vector<int> &nums)
+// {
+//     int i=0;
+//     for(int j=0;j<nums.size();j++)
+//     {
+//         if(nums[j]!=0) {swap(arr[i],arr[j]);
+//         i++;}
+//     }
+// }
