78ca3f213831a5e146da86f5ff8dc7a7838b31de
Sameer Basha <sameerndl28@gmail.com>
2024-05-07 11:42:49 +0530

    Sorting Algorithms

---
+++ SortingAlgorithms/bubbleSort.cpp
@@ -0,0 +1,12 @@
+/*
+ void bubbleSort(vector<int> &arr)
+    {
+        int n=arr.size();
+        for(int i=0;i<n-1;i++) { //for rounds n-1
+            
+            for(int j=0;j<n-1-i;j++)
+            {
+                if(arr[j]>arr[j+1]) swap(arr[j] , arr[j+1]);                 
+            }
+        }
+    }*/
