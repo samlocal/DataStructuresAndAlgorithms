e9c2cc7034e59822684f54d5caad73d42b6cc17e
Sameer Basha <sameerndl28@gmail.com>
2024-05-09 16:15:10 +0530

    2D Arrays and Problems

---
+++ TwoDimensionalArrays/spiral_pattern_print_2.cpp
@@ -0,0 +1,48 @@
+/*class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int count = 0;
+        int total = row * col;
+
+        // indexes
+        int startingRow = 0;
+        int startingCol = 0;
+        int endingRow = row - 1;
+        int endingCol = col - 1;
+
+        while (count < total) {
+            // print starting row
+            for (int index = startingCol; index <= endingCol && count < total; index++) {
+                ans.push_back(matrix[startingRow][index]);
+                count++;
+            }
+            startingRow++;
+
+            // print ending col
+            for (int index = startingRow; index <= endingRow && count < total; index++) {
+                ans.push_back(matrix[index][endingCol]);
+                count++;
+            }
+            endingCol--;
+
+            // printing ending row
+            for (int index = endingCol; index >= startingCol && count < total; index--) {
+                ans.push_back(matrix[endingRow][index]);
+                count++;
+            }
+            endingRow--;
+
+            // printing starting col
+            for (int index = endingRow; index >= startingRow && count < total; index--) {
+                ans.push_back(matrix[index][startingCol]);
+                count++;
+            }
+            startingCol++;
+        }
+        return ans;
+    }
+};*/
