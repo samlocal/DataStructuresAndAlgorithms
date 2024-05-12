7d35e951c49ce85719eb8c4bfd21be98ca609e3e
Sameer Basha <sameerndl28@gmail.com>
2024-05-12 18:01:34 +0530

    Pointers for DSA

--- Pointers/MacrosGlobalVariablesInlinefunctionsAndDefualtArgs.cpp
+++ Pointers/MacrosGlobalVariablesInlinefunctionsAndDefualtArgs.cpp
@@ -15,6 +15,7 @@ inline int getmax(int a, int b)
     return (a>b)?a:b;
 }
 int main()
+
 {
     int radius = 2;
     cout << "Area is " << PI * radius * radius << "\n"; // PI is replaced with 3.14 before compilation
