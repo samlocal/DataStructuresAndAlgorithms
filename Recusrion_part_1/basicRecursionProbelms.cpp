b27075801f261c3ecd692c2e403b444b9ffde9f3
Sameer Basha <sameerndl28@gmail.com>
2024-05-14 11:58:58 +0530

    Recursion Part 1

---
+++ Recusrion_part_1/basicRecursionProbelms.cpp
@@ -0,0 +1,21 @@
+//Functional Recursion 
+
+#include<iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    if(n==1) return 1;
+
+    return n*factorial(n-1);
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter n: ";
+
+    cin>>n;
+
+    cout<<"Factorial of "<<n<<" is "<<factorial(n);
+}
