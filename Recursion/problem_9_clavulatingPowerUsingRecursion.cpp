8f8a6be75c948e2de302dc115712c411250a59a0
Sameer Basha <sameerndl28@gmail.com>
2024-05-15 12:22:24 +0530

    Recursion Concept and Problems

--- Recursion/problem_9_clavulatingPowerUsingRecursion.cpp
+++ Recursion/problem_9_clavulatingPowerUsingRecursion.cpp
@@ -1,17 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+// int power(int a, int b)
+// {
+//     if (b == 1)
+//         return a; // or if(b==0) return 1;
+
+//     return a * power(a, b - 1);
+// }
 
 int power(int a, int b)
 {
-    if(b==1) return a;  // or if(b==0) return 1;
+    if(b==0) return 1;
+    
+    
 
-    return a*power(a,b-1);
+    if (b & 1)
+    { // odd case
+        return  a*power(a, b / 2) * power(a, b / 2);
+    }
+    else
+    { // even case
+        return power(a, b / 2) * power(a, b / 2);
+    }
 }
 int main()
 {
-    int a,b;
-    cout<<"Enter a and b : ";
-    cin>>a>>b;
+    int a, b;
+    cout << "Enter a and b : ";
+    cin >> a >> b;
 
-    cout<<"power of "<<a<<" and "<<b<<" is "<<power(a,b);
+    cout << "power of " << a << " and " << b << " is " << power(a, b);
 }
