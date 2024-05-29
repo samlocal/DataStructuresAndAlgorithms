096b8ddbd5b6da0af75e5689a9b9bef2519dfedb
Sameer Basha <sameerndl28@gmail.com>
2024-05-29 09:52:17 +0530

    Stack and related problems

---
+++ Stack/problem_2_reverseStringUsingStack.cpp
@@ -0,0 +1,23 @@
+
+/*class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        
+        stack<char> st;
+
+        for(int i=0;i<s.size();i++)
+        {
+            char ch=s[i];
+            st.push(ch);
+        }
+
+        s.clear();
+        while(!st.empty())
+        {
+            char ch=st.top();
+            s.push_back(ch);
+            st.pop();
+        }
+    }
+};
+*/
