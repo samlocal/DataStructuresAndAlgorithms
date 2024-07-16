375821957e8c5e2796876b0dc82a9f9595e3a6d4
Sameer Basha <sameerndl28@gmail.com>
2024-07-16 10:13:54 +0530

    Binary Search Tree Prblems

--- BinarySearchTree/problem_1_searchInBST.cpp
+++ BinarySearchTree/problem_1_searchInBST.cpp
@@ -15,6 +15,8 @@
 */
 
 
+
+//Recursive Way:
 /*
 bool searchInBST(BinaryTreeNode<int> *root, int x) {
     if(root==NULL) return false;
@@ -28,3 +30,22 @@ bool searchInBST(BinaryTreeNode<int> *root, int x) {
 
 }
 */
+
+//Iterative Way:
+/*
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+    BinaryTreeNode<int> *temp=root;
+
+    while(temp!=NULL)
+    {
+        if(temp->data==x) return true;
+
+        if(temp->data>x)
+        {
+            temp=temp->left;
+        }
+
+        else temp=temp->right;
+    }
+    return false;
+}*/
