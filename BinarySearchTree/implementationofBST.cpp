30c7252e72756ac531043be9c4174b1ef16cbf57
Sameer Basha <sameerndl28@gmail.com>
2024-07-16 11:03:09 +0530

    Binary Search Tree Prblems

--- BinarySearchTree/implementationofBST.cpp
+++ BinarySearchTree/implementationofBST.cpp
@@ -102,6 +102,53 @@ int maxValue(Node* root)
 
     return temp->data;
 }
+
+Node* deleteFromBST(Node* &root,int val)
+{
+    if(root==NULL) return NULL;
+
+    if(root->data==val)
+    {
+        //0 child
+        if(root->left==NULL && root->right==NULL) {
+            delete root;
+            return NULL;
+        }
+        //1 child
+        //having left child
+        if(root->left!=NULL && root->right==NULL)
+        {
+            Node* temp=root->left;
+            delete root;
+            return temp;
+        }
+         //having right child
+        if(root->right!=NULL && root->left==NULL)
+        {
+            Node* temp=root->right;
+            delete root;
+            return temp;
+        }
+        //2 child
+        if(root->left!=NULL && root->right!=NULL)
+        {
+            int minVal=minValue(root->left); //finding min value
+            root->data=minVal; //replacing with value
+            root->left=deleteFromBST(root->left,minVal); //deleting that node
+            return root;
+        }
+    }
+    else if(root->data>val) //left part
+    {
+        root->left=deleteFromBST(root->left,val);
+        return root;
+    }
+    else{ //right part
+        root->right=deleteFromBST(root->right,val);
+        return root;
+    }
+
+}
 int main()
 {
     Node* root=NULL;  //root node
@@ -119,5 +166,7 @@ int main()
 
     //Maximim Value
     cout<<"Maximum Value : "<<maxValue(root)<<"\n";
+
+    
     
 }
