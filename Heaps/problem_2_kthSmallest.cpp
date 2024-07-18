b582cbe112c9c3e5f4c7315897a557a99626495d
Sameer Basha <sameerndl28@gmail.com>
2024-07-18 20:50:55 +0530

    Binary Search Tree Prblems

---
+++ Heaps/problem_2_kthSmallest.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        priority_queue<int> max_heap;
+        
+        //pushing first k elements into max heap
+        for(int i=0;i<k;i++)
+        {
+            max_heap.push(arr[i]);
+        }
+        
+        //for remaining elements
+        for(int i=k;i<r;i++)
+        {
+            if(arr[i]<max_heap.top())
+            {
+                max_heap.pop();
+                max_heap.push(arr[i]);
+            }
+        }
+    
+        
+        return max_heap.top();
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+ 
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
