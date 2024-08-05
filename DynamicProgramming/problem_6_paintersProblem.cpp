ec17314183e13204930e8fc794e619c2cc8c43f5
Sameer Basha <sameerndl28@gmail.com>
2024-08-05 15:52:49 +0530

    Dynamic Programming

---
+++ DynamicProgramming/problem_6_paintersProblem.cpp
@@ -0,0 +1,114 @@
+//Using Rec
+/*#include <bits/stdc++.h> 
+#define MOD 1000000007
+int add(int a,int b)
+{
+    return (a%MOD + b%MOD)%MOD;
+}
+int mul(int a,int b)
+{
+    return (a%MOD * b%MOD)%MOD;
+}
+int solve(int n,int k)
+{
+    if(n==1) return k;
+
+    if(n==2) return add(k,mul(k,k-1));
+
+    int ans=add(mul(solve(n-2,k),k-1), mul(solve(n-1,k),k-1));
+    return ans;
+}
+int numberOfWays(int n, int k) {
+    return solve(n ,k);
+}
+***********************************/
+//Using rec + mem
+/*
+#include <bits/stdc++.h> 
+#define MOD 1000000007
+int add(int a,int b)
+{
+    return (a%MOD + b%MOD)%MOD;
+}
+int mul(int a,int b)
+{
+    return (a%MOD *1LL* b%MOD)%MOD;
+}
+
+int solveRecMem(int n ,int k ,vector<int>&dp)
+{
+    if(n==1) return k;
+
+    if(n==2) return add(k,mul(k,k-1));
+
+    if(dp[n]!=-1) return dp[n];
+
+    dp[n]=add(mul(solveRecMem(n-2,k,dp),k-1), mul(solveRecMem(n-1,k,dp),k-1));
+    return dp[n];
+}
+int numberOfWays(int n, int k) {
+    vector<int>dp(n+1,-1);
+    return solveRecMem(n ,k,dp);
+}
+***************************************/
+//Using Tabulation
+/*
+#include <bits/stdc++.h> 
+#define MOD 1000000007
+int add(int a,int b)
+{
+    return (a%MOD + b%MOD)%MOD;
+}
+int mul(int a,int b)
+{
+    return (a%MOD *1LL* b%MOD)%MOD;
+}
+int Tabulation(int n ,int k)
+{
+    vector<int>dp(n+1 , 0);
+
+    dp[1]=k;
+    dp[2]=add(k,mul(k,k-1));
+
+    for(int i=3;i<=n;i++)
+    {
+        dp[i]=add(mul(dp[i-2],k-1), mul(dp[i-1],k-1));
+    }
+    return dp[n];
+}
+int numberOfWays(int n, int k) {
+   
+    return Tabulation(n, k);
+}
+***************************************/
+//Using Space Optimization
+/*
+#include <bits/stdc++.h> 
+#define MOD 1000000007
+int add(int a,int b)
+{
+    return (a%MOD + b%MOD)%MOD;
+}
+int mul(int a,int b)
+{
+    return (a%MOD *1LL* b%MOD)%MOD;
+}
+
+int spaceOptimization(int n,int k)
+{
+    int prev2=k;
+    int prev1=add(k,mul(k,k-1));
+    for(int i=3;i<=n;i++)
+    {
+        int ans=add(mul(prev2,k-1), mul(prev1,k-1));
+        prev2=prev1;
+        prev1=ans;
+    }
+    return prev1;
+}
+int numberOfWays(int n, int k) {
+   
+    return spaceOptimization(n, k);
+}
+
+***************************************************/
