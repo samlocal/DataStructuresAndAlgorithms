00c0bb891bb9effe17f8d5a032b8dbe024f6b56f
Sameer Basha <sameerndl28@gmail.com>
2024-08-08 13:02:06 +0530

    Dynamic Programming

---
+++ DynamicProgramming/problem_12_maximalSquares.cpp
@@ -0,0 +1,153 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+  int solve(vector<vector<int>> &mat , int i,int j , int &maxi)
+  {
+      if(i>=mat.size() || j>=mat[0].size())
+      {
+          return 0;
+      }
+      
+      int right=solve(mat,i,j+1,maxi);
+      int diagonal=solve(mat,i+1,j+1,maxi);
+      int down=solve(mat,i+1,j,maxi);
+      
+      if(mat[i][j]==1)
+      {
+          int ans=1+min(right , min(down,diagonal));
+          maxi=max(maxi,ans);
+          return ans;
+      }
+      else return 0; //m[i][j]==0
+  }
+  int solveRecMem(vector<vector<int>> &mat , int i,int j , int &maxi,vector<vector<int>> &dp)
+  {
+      
+      if(i>=mat.size() || j>=mat[0].size())
+      {
+          return 0;
+      }
+      
+      if(dp[i][j]!=-1) return dp[i][j];
+      
+      int right=solveRecMem(mat,i,j+1,maxi,dp);
+      int diagonal=solveRecMem(mat,i+1,j+1,maxi,dp);
+      int down=solveRecMem(mat,i+1,j,maxi,dp);
+      
+      if(mat[i][j]==1)
+      {
+          int ans=1+min(right , min(down,diagonal));
+          maxi=max(maxi,ans);
+          dp[i][j]=ans;
+          return dp[i][j];
+      }
+      else return dp[i][j]=0; //m[i][j]==0
+  }
+  int solveTab(vector<vector<int>> &mat , int &maxi)
+  {
+      int row=mat.size();
+      int col=mat[0].size();
+      vector<vector<int>>dp(row+1,vector<int>(col+1,0));
+      
+      for(int i=row-1;i>=0;i--)
+      {
+          for(int j=col-1;j>=0;j--)
+          {
+        int right=dp[i][j+1];
+      int diagonal=dp[i+1][j+1];
+      int down=dp[i+1][j];
+      
+      if(mat[i][j]==1)
+      {
+          dp[i][j]=1+min(right , min(down,diagonal));
+          
+          maxi=max(maxi,dp[i][j]);
+         
+      }
+      else  dp[i][j]=0; //m[i][j]==0
+          }
+      }
+      return dp[0][0];
+  }
+ int spaceOptimization(vector<vector<int>> &mat, int &maxi) {
+    int row = mat.size();
+    int col = mat[0].size();
+    vector<int> curr(col + 1, 0);
+    vector<int> next(col + 1, 0);
+
+    for (int i = row - 1; i >= 0; i--) {
+        for (int j = col - 1; j >= 0; j--) {
+            int right = curr[j + 1];
+            int diagonal = next[j + 1];
+            int down = next[j];
+
+            if (mat[i][j] == 1) {
+                curr[j] = 1 + min(right, min(down, diagonal));
+                maxi = max(maxi, curr[j]);
+            } else {
+                curr[j] = 0; // mat[i][j] == 0
+            }
+        }
+        next = curr;
+    }
+
+    return next[0];
+}
+
+    int maxSquare(int n, int m, vector<vector<int>> mat) {
+        /*Recursion
+        int maxi=0;
+        solve(mat,0,0,maxi);
+        return maxi;
+        */
+        
+        /*Recursion+memorization
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        int maxi=0;
+        solveRecMem(mat,0,0,maxi,dp);
+        return maxi;
+        */
+        
+        /*Tabulation
+        int maxi=0;
+         solveTab(mat,maxi);
+         return maxi;
+         */
+         
+         /*Space Optimization
+         int maxi=0;
+         spaceOptimization(mat,maxi);
+        return maxi;
+          
+          */
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> mat(n, vector<int>(m, 0));
+        for (int i = 0; i < n * m; i++)
+            cin >> mat[i / m][i % m];
+
+        Solution ob;
+        cout << ob.maxSquare(n, m, mat) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
